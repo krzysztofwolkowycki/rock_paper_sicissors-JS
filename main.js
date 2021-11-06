27e080ec831fef3b0e5dc0b259a93d161d9ecc9a
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2021-11-06 10:17:43 +0100

    ShowResult

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function aiChoice(){
 function checkResult(player, ai){
     if(player === ai){
         console.log('Remis');
-        return 'draw'
+        return 'draws'
     } else if (player === 'kamień' && ai === 'nożyczki' || player === 'papier' && ai === 'kamień' || player === 'nożyczki' && ai === 'papier'){
         console.log('wygrałeś');
         return 'wins'
@@ -37,8 +37,23 @@ function checkResult(player, ai){
     }
 }
 function showReult(player, ai, result){
+    document.querySelector('p.numbers span').textContent = ++summaryGame.number;
     document.querySelector('[data-summary="your-choice"]').textContent = player;
-    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+    if(result === 'wins'){
+        document.querySelector('[data-summary="who-win"]').textContent = 'Wygrałeś';
+        document.querySelector('p.wins span').textContent = ++summaryGame.wins;
+        document.querySelector('[data-summary="who-win"]').style.color = 'green';
+    }else if(result === 'losses'){
+        document.querySelector('[data-summary="who-win"]').textContent = 'Przegrałeś :(';
+        document.querySelector('p.losses span').textContent = ++summaryGame.losses;
+        document.querySelector('[data-summary="who-win"]').style.color = 'red';
+    }else{
+        document.querySelector('[data-summary="who-win"]').textContent = 'Remis';
+        document.querySelector('p.draws span').textContent = ++summaryGame.draws;
+        document.querySelector('[data-summary="who-win"]').style.color = 'gray';
+    }
+
 }
 
 
@@ -49,7 +64,6 @@ function startGame(){
     }
     aiChoice();
     const result = checkResult(game.playerHand, game.aiHand);
-    
     showReult(game.playerHand, game.aiHand, result);
 }
 
